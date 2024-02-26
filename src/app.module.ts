4d5df5bcc4b4af6c9ea7c328bef4ae934e6eb6ed
dapdy <promise@listtrader.com>
2024-02-27 00:50:04 +0200

    Revert brand

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { BatchCodeDecodingResultModule } from './batch-code-decoding-result/batc
 import { BatchCodeFormularModule } from './batch-code-formular/batch-code-formular.module';
 import { ManufacturerModule } from './manufacturer/manufacturer.module';
 import { UserFeedbackModule } from './user-feedback/user-feedback.module';
+import { BrandModule } from './brand/brand.module';
 
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, BatchCodeDecodingResultModule, BatchCodeFormularModule, ManufacturerModule, UserFeedbackModule],
+  imports: [PrismaModule, ArticlesModule, BatchCodeDecodingResultModule, BatchCodeFormularModule, ManufacturerModule, UserFeedbackModule, BrandModule],
   controllers: [AppController],
   providers: [AppService],
 })
