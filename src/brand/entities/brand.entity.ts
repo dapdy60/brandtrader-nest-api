e1468302396bc6d41cabd0fef5116861d66bda69
dapdy <promise@listtrader.com>
2024-02-27 00:49:54 +0200

    Revert entity

---
+++ src/brand/entities/brand.entity.ts
@@ -0,0 +1,39 @@
+import { Brand } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+export class BrandEntity implements Brand {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty()
+    creationTime: Date;
+
+    @ApiProperty()
+    creatorId: string;
+
+    @ApiProperty()
+    lastModificationTime: Date;
+
+    @ApiProperty()
+    lastModifierId: string;
+
+    @ApiProperty()
+    isDeleted: boolean;
+
+    @ApiProperty()
+    deleterId: string;
+
+    @ApiProperty()
+    deletionTime: Date;
+
+    @ApiProperty()
+    manufacturerId: string;
+
+    @ApiProperty()
+    fullName: string;
+
+    @ApiProperty()
+    standardName: string;
+
+    @ApiProperty()
+    logoUrl: string;
+}
